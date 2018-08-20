a41eaf202836780d956e627d1958059cc108270c
Morris Allison III <morris.allison@autodesk.com>
2018-08-20 11:01:59 -0700

    chore: add `subject` param to `takeSnapshotsOf`

--- packages/jest-preset/helpers/takeSnapshotsOf.js
+++ packages/jest-preset/helpers/takeSnapshotsOf.js
@@ -1,10 +1,34 @@
 const React = require("react");
 const renderer = require("react-test-renderer");
 
-module.exports = function takeSnapshotsOf(Component, cases, rendererOptions) {
-  cases.forEach(({ desc, props }) => {
+/**
+ * @typedef {import("react-test-renderer").TestRendererOptions} Options
+ */
+
+/**
+ * @typedef {Object} Case
+ * @property {string} desc
+ * @property {Object} [props]
+ * @property {import("react").ReactElement} [subject]
+ */
+
+/** @type {Options} */
+const defaultOptions = {
+  createNodeMock(element) {
+    return document.createElement(element.type);
+  }
+};
+
+/**
+ * @param {import("react").Component|null} Component
+ * @param {Case[]} cases
+ * @param {Options} [options]
+ */
+module.exports = function takeSnapshotsOf(Component, cases, options) {
+  cases.forEach(({ desc, props, subject }) => {
     it(desc, () => {
-      const vDom = React.createElement(Component, props);
+      const rendererOptions = Object.assign({}, defaultOptions, options);
+      const vDom = subject || React.createElement(Component, props);
       const tree = renderer.create(vDom, rendererOptions).toJSON();
 
       expect(tree).toMatchSnapshot();
